46911c3c1503a5c607f4ff0c5c6f1aa2fcf2e5f4
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-08-13 22:01:44 +0530

    fix morque

--- src/components/MovingText.jsx
+++ src/components/MovingText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MovingText = () => {
   return (
     <div className="w-full overflow-hidden bg-pink-200 py-2">
-      <p className="whitespace-nowrap text-red-800 text-lg font-bold animate-marquee">
+      <p className="whitespace-nowrap text-red-800 text-xl font-bold animate-marquee">
         🏥 Welcome to Kaushalya Clinic — Your Trusted Partner in Wellness —
         Quality Care | Experienced Doctors | 24/7 Support 🏥
       </p>
